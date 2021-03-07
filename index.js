8461faf2f308135deb5bccc8f4258467957ae929
Nelson Chen <crazysim@gmail.com>
2021-03-06 17:02:53 -0700

    Far simpler layout

--- index.js
+++ index.js
@@ -1,12 +1,87 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
+const htmlHeader = { 'content-type': 'text/html' }
+const htmlStyle = `
+<style>
+        body {
+            max-width: 38rem;
+            padding: 2rem;
+            margin: auto;
+        }
+</style>
+`
+
+const frontPageHtml = `
+<html lang='en'>
+<head>
+<title>GitHub Wiki SEE</title>
+${htmlStyle}
+</head>
+<body id='main'>
+    <h1>GitHub Wiki SEE</h1>
+    <p>
+      GitHub Wiki Search Engine Enablement is a tool to allow GitHub Wikis to
+      be indexed by search engines. There's no <code>robots.txt</code> on this
+      site. Because of this, text content served from this tool should be 
+      indexed.
+    </p>
+    <p>
+      Currently, <a href='https://github.com/isaacs/github/issues/1683'>
+      GitHub prevents crawling of repository's Wiki pages. This means Wikis are
+      invisible to search engines.</a> This can be a very unpleasant surprise 
+      if you're looking to share knowledge or put a FAQ on a Wiki!
+    </p>
+    <p>
+      This tool simply mirrors the rendered contents of the pages and has a link
+      at the top to visit the page on GitHub for normal usage. The content of 
+      the tool is not meant to be read by users. A link is put above on the 
+      mirrored and index-able pages to GitHub for actual reading and editing.
+    </p>
+    <h2>Usage</h2>
+    <ol>
+        <li>
+          Go to this site at 
+          <code>/ghw/{username or organization}/{repo}</code>
+        </li>
+        <li>
+          Make sure the page has content.
+        </li>
+        <li>
+          For best results, place the link in a location that can be indexed 
+          by search engines such as a README or some other place.
+        </li>
+    </ol>
+    <h2>Project Page</h2>
+      <p><a href="https://github.com/nelsonjchen/github-wiki-see">GitHub</a></p>
+    <h2>Examples / My Seeds</h2>
+    <ul>
+        <li>
+          <a href='/ghw/nelsonjchen/github-wiki-test'>
+            nelsonjchen/github-wiki-test
+          </a>
+        </li>
+        <li>
+          <a href='/ghw/commaai/openpilot'>
+            commaai/openpilot
+          </a>
+        </li>
+    </ul>
+</body>
+</html>
+`
+
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
+  console.log(request.pathname)
+  if (request.pathname === '/' || request.pathname === undefined) {
+    return new Response(frontPageHtml,
+      {
+        headers: htmlHeader,
+      })
+  }
+
+  return new Response('Hello worker!!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
